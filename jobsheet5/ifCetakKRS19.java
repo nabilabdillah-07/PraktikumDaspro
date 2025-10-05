5443abe1cd7ebb37147eecde80483f7a0b1221e2
nabilabdillah-07 <nabil.abdillah.me@gmail.com>
2025-10-05 19:33:30 +0700

    Tugas 1

--- jobsheet5/ifCetakKRS19.java
+++ jobsheet5/ifCetakKRS19.java
@@ -9,12 +9,10 @@ public class ifCetakKRS19 {
         System.out.println("Apakah UKT sudah lunas? (True/false): ");
         boolean uktLunas = sc.nextBoolean();
 
-        if (uktLunas) {
-            System.out.println("Pembayaran UKT terverifikasi");
-            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
-        } else {
-             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
-
+         String hasil = uktLunas 
+            ? "Pembayaran UKT terverifikasi\nSilahkan cetak KRS dan minta tanda tangan DPA" 
+            : "Registrasi ditolak. Silakan lunasi UKT terlebih dahulu";
+        
+        System.out.println(hasil);
         }
     }
-}
