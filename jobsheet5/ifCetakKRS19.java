0e9988717626fc24078c727c16d3a8dfe391f47f
nabilabdillah-07 <nabil.abdillah.me@gmail.com>
2025-10-05 17:43:07 +0700

    Modifikasi percobaan 1

---
+++ jobsheet5/ifCetakKRS19.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ifCetakKRS19 {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("--- Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (True/false): ");
+        boolean uktLunas = sc.nextBoolean();
+
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+             System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
+
+        }
+    }
+}
